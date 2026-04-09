d8d8b8928802a185907c552e7fbd5bb5ad36e023
Hlib Hrynchenko <grigler999@gmail.com>
2026-04-09 18:55:08 +0200

    feat: initialize C version with basic menu output

---
+++ networktool.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("= NÄTVERKSVERKTYG =\n");
+    printf("1. Validera IP-adress\n");
+    printf("2. Validera port\n");
+    printf("3. Visa logg\n");
+    printf("4. Avsluta\n");
+
+    return 0;
+}
